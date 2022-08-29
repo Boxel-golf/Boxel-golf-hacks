9a4eefd2699056536eb2faf67ecd65b3b771e84a
Boxel-golf <oscar.childs3@gmail.com>
2022-08-29 15:03:24 +1200

    Update Custom.js

--- Custom.js
+++ Custom.js
@@ -1,7 +1,7 @@
 custom_hat = function() {
-    app.assets.models.children[119] = app.assets.models.copy(app.assets.models.children[index])
+    app.assets.models.children[119] = app.assets.models.copy(app.assets.models.children[69])
     app.assets.models.children[119].name = 'hat-custom'
-    app.assets.models.children[119].userData = app.assets.models.children[69].userData
+    app.assets.models.children[119].userData = app.assets.models.children[48].userData
     app.assets.models.children[119].userData.order = 16
 }
 
